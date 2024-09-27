4805f011edb4edb38612292de44432f606e5c1af
Daniel <Daniel.Petersson83@gmail.com>
2024-09-27 20:23:50 +0200

    added validation

--- aseda-jvf/src/main/java/se/leiden/asedajvf/model/Member.java
+++ aseda-jvf/src/main/java/se/leiden/asedajvf/model/Member.java
@@ -1,7 +1,11 @@
 package se.leiden.asedajvf.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import se.leiden.asedajvf.enums.Role;
 
 import java.time.LocalDate;
@@ -19,17 +23,32 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     private String firstName;
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
     private String lastName;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+    @Pattern(regexp = "^\\+?\\d{10,14}$", message = "Invalid phone number")
     private String phone;
+    @NotBlank(message = "Address is required")
     private String address;
+    @NotBlank(message = "City is required")
     private String city;
     @Enumerated(EnumType.STRING)
     private Role role;
+    @Min(value = 10000, message = "Invalid postal code")
+    @Max(value = 99999, message = "Invalid postal code")
     private int postalCode;
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
+    @CreatedDate
     private LocalDate dateCreated;
+    @UpdateTimestamp
     private LocalDate dateUpdated;
     private LocalDate membershipPaidUntil;
     private boolean active;
